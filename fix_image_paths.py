57384ffcdbf1853450e131e3d7dc8b3be81baa84
khaivandangusf2210 <156172346+khaivandangusf2210@users.noreply.github.com>
2025-06-05 01:37:53 -0400

    Delete fix_image_paths.py

--- fix_image_paths.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('old_processed_fixed.csv')
-df['image_path'] = df['image_path'].str.replace(r'^.*?/', 'old_processed/', regex=True)
-df.to_csv('old_processed_fixed.csv', index=False)
-print('Updated image_path to use old_processed/.') 
